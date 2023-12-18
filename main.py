283ae39933a393cf10f9dcf09ae81abf10db7bf4
Kirsanov Kirill <119342859+sotonoche@users.noreply.github.com>
2023-12-19 00:02:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,17 +6,21 @@ fi = (p-1)*(q-1)
 
 e = int(input('Введите число [e]: '))
 
-d = 0
+D = 0
 i = 1
 
-while True:
-    if (e*i) % fi == 1:
-        d = i
-        break
-    else:
-        i += 1
 
-print(f'\nE = ({e}, {n}) D = ({d}, {n}\n)')
+"""
+Поиск числа D
+"""
+while True:
+    if not (e*i) % fi == 1:
+        i += 1
+    else:
+        D = i
+        break
+        
+print(f'\nE = ({e}, {n}) D = ({D}, {n}\n)')
 s = int(input('Введите [s]: '))
 t = pow(s, e, n)
-print(f'Борис получает t, равное {t} и находит s, которое равно {pow(t, d, n)}')
+print(f'Борис получает t, равное {t} и находит s, которое равно {pow(t, D, n)}')
